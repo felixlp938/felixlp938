34138821e6e956ac4335d80e8b280dc1db24d2a5
felixlp938 <85683853+felixlp938@users.noreply.github.com>
2021-06-27 17:31:30 +0200

    Add files via upload
    Dieses Game ist eine Art Glücksspiel, wo man eine Zahl zwischen 1 und 5 eingeben muss. Wenn die eingegebene Zahl mit der generierten Zahl übereinstimmt, hast du gewonnen. Ansonsten verloren.
    -------------------------------------------------------
    SYSTEMANFORDERUNGEN:
    
    - Python (Download: https://www.python.org/downloads/)

---
+++ gluecksspiel.py
@@ -0,0 +1,13 @@
+print("Dieses Game ist ein Glücksspiel  -  Gebe eine Zahl zwischen 1 und 5 ein, und gucke ob sie richtig ist  -:-  Gecodet von Felix [github.com/felixlp938]")
+
+eing = input("Gebe hier die Zahl ein:")
+
+from random import *
+rand = randint(1, 5)
+
+print ("Zufallszahl:", rand)
+
+if str(eing) == str(rand):
+    print("Du hast gewonnen")
+else: 
+    print("Du hast leider verloren oder eine falsche Zahl eingegeben :(")
