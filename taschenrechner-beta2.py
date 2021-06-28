1c86cdd733f35cc9ef14656a1df47f27b10e08ec
felixlp938 <85683853+felixlp938@users.noreply.github.com>
2021-06-28 13:52:15 +0200

    Add files via upload
    Taschenrechner Beta Version 2.
    
    ----------
    
    Systemanforderungen:
    
    Python (Download: python.org/downloads)

---
+++ taschenrechner-beta2.py
@@ -0,0 +1,42 @@
+print("Taschenrechner 2.0 - GECODET VON FELIX")
+
+print("Als 1. Option ist die Addition, dann Subtraktion, dann Multiplikation, und zum Schluss die Division.")
+print("WENN MAN EINE OPTION SKIPPEN WILL, MUSS MAN EINE 0 (NULL) EINGEBEN.")
+print("DANKE FÜR DAS NUTZEN MEINES ERSTEN TASCHENRECHNERS. AN EINER FINALEN VERSION WIRD NOCH GEARBEITET!")
+print("WICHTIG: BITTE ANSTATT EINES KOMMAS (,) EINEN PUNKT MACHEN (.) SONST FUNLTIONIERT ES NICHT!!")
+print("ADDITION:")
+
+add1 = float(input("BITTE DIE ERSTE ZAHL DER ADDITION EINGEBEN:"))
+add2 = float(input("BITTE DIE ZWEITE ZAHL DER ADDITION EINGEBEN:"))
+
+print("Es wird nun die Addition berechnet....")
+print("DONE! Das Ergebniss ist:", add1 + add2)
+
+
+print("SUBTRAKTION:")
+
+sub1 = float(input("BITTE DIE ERSTE ZAHL DER SUBTRAKTION EINGEBEN:"))
+sub2 = float(input("BITTE DIE ZWEITE ZAHL DER SUBTRAKTION EINGEBEN:"))
+
+print("Es wird nun die Subtraktion berechnet....")
+print("DONE! Das Ergebniss ist:", sub1 - sub2)
+
+
+print("MULTIPLIKATION:")
+
+mul1 = float(input("BITTE DIE ERSTE ZAHL DER MULTIPLIKATION EINGEBEN:"))
+mul2 = float(input("BITTE DIE ZWEITE ZAHL DER MULTIPLIKATION EINGEBEN:"))
+
+print("Es wird nun die Multiplikation berechnet....")
+print("DONE! Das Ergebniss ist:", mul1 * mul2)
+
+
+print("DIVISION")
+
+div1 = float(input("BITTE DIE ERSTE ZAHL DER DIVISION EINGEBEN:"))
+div2 = float(input("BITTE DIE ZWEITE ZAHL DER DIVISION EINGEBEN:"))
+
+print("Es wird nun die Division berechnet....")
+print("DONE! Das Ergebniss ist:", div1 / div2)
+
+input("Enter zum Beenden drücken")
